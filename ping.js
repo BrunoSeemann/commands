329df3dde52d376441475d80c528d2c0255ea75b
Bruno Seemann <65774228+BrunoSeemann@users.noreply.github.com>
2020-07-20 20:04:04 -0300

    Add files via upload

---
+++ ping.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+
+
+module.exports.run = async (bot, message, args) => {
+
+    message.channel.send("Pinging...").then(m => {
+        let ping = m.createdTimestamp - message.createdTimestamp
+        let choices = ["Is this really my ping", "Is it okay? I can't look", "I hope it isn't bad"]
+        let response = choices[Math.floor(Math.random() * choices.length)]
+
+        m.edit(`${response}: Bot Latency: \`${ping}\`, API Latency: \`${Math.round(bot.ping)}\``)
+    })
+
+}
+
+module.exports.config = {
+    name: "ping",
+    aliases: ["latency"],
+    usage: "%ping",
+    description: "PONG! Displays the API & BOT latency",
+    acessableby: "Members"
+}
